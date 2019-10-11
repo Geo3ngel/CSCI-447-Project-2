3b4185b7144ab6e1d4d28c1b4650d535fc4fb4b5
Henry Soule <hsoule427@gmail.com>
2019-10-10 23:41:23 -0600

    More main_execution updates.

--- src/main.py
+++ src/main.py
@@ -95,10 +95,7 @@ def main_execution():
     k_medoid_regress_avgs = []
     
     reduction_funcs = [
-        'edited_nn',
-        'condensed_nn',
-        'k_means',
-        'k_medoids'
+        'edited_nn'
     ]
 
     # Initializes path manager with default directory as databases.
@@ -111,40 +108,63 @@ def main_execution():
 
     for database in selected_dbs:
         db = prepare_db(database, pm)
-    
-    # Start k-fold cross validation
-    print("RUNNING K-FOLD CROSS VALIDATION")
-    # Prepare data for k-fold
-    binned_data, bin_lengths = process_data.separate_data(db.get_attr(), db.get_data())
-    # Extract validation set
-    bin_lengths, validate_data, binned_data = validate.get_validate(bin_lengths, binned_data)
-    debug_file.write('\n\nVALIDATION DATA: \n')
-    for row in validate_data:
-        debug_file.write(str(row) + '\n')
+        print(db.get_dataset_type())        
+        # k_nearest = knn(5, db.get_dataset_type(), \
+        #         db.get_classifier_col(), \
+        #         db.get_classifier_attr_cols())
 
-    knn = knn(5, db.get_dataset_type(), \
-              db.get_classifier_col(), \
-              db.get_classifier_attr_cols())
-    #NOTE binned_data needs to still be shuffled somewhere above here
+        # # Start k-fold cross validation
+        # print("RUNNING K-FOLD CROSS VALIDATION")
+        # # Prepare data for k-fold
+        # binned_data, bin_lengths = process_data.separate_data(db.get_attr(), db.get_data())
+        # # Extract validation set
+        # bin_lengths, validate_data, binned_data = validate.get_validate(bin_lengths, binned_data)
+        # debug_file.write('\n\nVALIDATION DATA: \n')
+        # for row in validate_data:
+        #     debug_file.write(str(row) + '\n')
+        #     #NOTE binned_data needs to still be shuffled somewhere above here
 
-    # Run k-fold on just k-means first
-    k_fold_results = validate.k_fold(9, binned_data, \
-                                    validate_data, bin_lengths, \
-                                    db, False, db.get_dataset_type(), \
-                                    knn, debug_file, output_file)
+        # # Run k-fold on just k-means first
+        # k_fold_results = validate.k_fold(9, binned_data, \
+        #                                 validate_data, bin_lengths, \
+        #                                 db, True, db.get_dataset_type(), \
+        #                                 k_nearest, debug_file, output_file,)
+        
+        if db.get_dataset_type() == 'classification':
+            k_nn_classification_avgs.append(sum(k_fold_results) / len(k_fold_results))
+        else:
+            k_nn_regress_avgs.append(sum(k_fold_results) / len(k_fold_results))
+
+        output_file.write('\n\n\n')
+        # Loop thru all reduction functions
+        for func in reduction_funcs:
+            print('RUNNING ', func)
+            #NOTE this bitch will probs have to use 9 instead of 10 because 
+            # we are removing a bin from bin_lengths
+            k_fold_results = validate.k_fold(9, binned_data, \
+                                            validate_data, bin_lengths, db, \
+                                            True, db.get_dataset_type(), \
+                                            k_nearest, debug_file, output_file, 'edited_nn')
+            if func == 'edited_nn':
+                enn_avgs.append(sum(k_fold_results) / len(k_fold_results))
+            elif func == 'condensed_nn':
+                cnn_avgs.append(sum(k_fold_results) / len(k_fold_results))
+            
+            output_file.write('K FOLD RESULTS: ' + str(k_fold_results))
+            
+            output_file.write('\n\n\n')
+            
+        print("KNN CLASSIFICATION AVGS: ", k_nn_classification_avgs)
+        # output_file.write("KNN CLASSIFICATION AVGS: " + k_nn_classification_avgs)
+        # debug_file.write("KNN CLASSIFICATION AVGS: " + k_nn_classification_avgs)
+        print("KNN REGRESSION AVGS: ", k_nn_regress_avgs)
+        print("ENN AVGS: ", enn_avgs)
+        print("CNN AVGS: ", cnn_avgs)
+        print("K MEANS CLASSIFICATION AVGS: ", k_means_classification_avgs)
+        print("K MEANS REGRESSION AVGS: ", k_means_regress_avgs)
+        print("K MEDOID CLASSIFICATION AVGS: ", k_medoid_classification_avgs)
+        print("K MEDOID REGRESSION AVGS", k_medoid_regress_avgs)
     
-    if db.get_dataset_type == 'classification':
-        k_nn_classification_avgs.append(sum(k_fold_results) / len(k_fold_results))
-    else:
-        k_nn_regress_avgs.append(sum(k_fold_results) / len(k_fold_results))
-    
-    for func in reduction_funcs:
-        #NOTE this bitch will probs have to use 9 instead of 10 because 
-        # we are removing a bin from bin_lengths
-        validate.k_fold(9, binned_data, \
-                        validate_data, bin_lengths, db, \
-                        False, db.get_dataset_type(), \
-                        knn, debug_file, output_file, 'edited_nn')
 
 
 
